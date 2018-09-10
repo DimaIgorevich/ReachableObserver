63eb9a2aef7265e17787d42fba90982f23b90cf0
dima <dzhimasan@gmail.com>
2018-09-10 18:36:57 +0300

    Initial

---
+++ ReachableObserver/ReachableObserver/ReachableObserver.h
@@ -0,0 +1,19 @@
+//
+//  ReachableObserver.h
+//  ReachableObserver
+//
+//  Created by dima on 10.09.2018.
+//  Copyright © 2018 com.QuilCode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReachableObserver.
+FOUNDATION_EXPORT double ReachableObserverVersionNumber;
+
+//! Project version string for ReachableObserver.
+FOUNDATION_EXPORT const unsigned char ReachableObserverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReachableObserver/PublicHeader.h>
+
+
